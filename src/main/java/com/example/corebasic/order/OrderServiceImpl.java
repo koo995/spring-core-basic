dfd1362b375b5002499534b0a8914a795d0ef195
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-07 00:40:16 +0900

    새로운 할인 정책 적용과 문제점2

--- src/main/java/com/example/corebasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/corebasic/order/OrderServiceImpl.java
@@ -16,8 +16,12 @@ public class OrderServiceImpl implements OrderService {
      * 정책을 변경할려면 FixDiscountPolicy 를 RateDiscountPolicy 로 변경해야 한다.
      * 추상 인터페이스 뿐만 아니라, 구체 클래스에도 의존하고 있다. 둘다 의존하고 있다 -> DIP 위반
      * 이 정도면 훌륭한 것이지만... 할인 정책 변경인데 OrderServiceImpl 코드를 고쳐야 한다. -> OCP 위반 (기능을 확장해서 변경하면, 클라이언트 코드에 영향을 준다.)
+     * 인터페이스에만 의존하도록 코드를 변경했다.
+     * 그러나 구현체가 없다....!! -> NullPointException 발생
+     * 이 문제를 해결하려면 어떻게 해야할까?
+     * 누군가가 구현체를 넣어줘야 한다. -> AppConfig(관심사의 분리를 위해 공연 기확자를 만든다)
      */
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy;
 
     /**
      * 단일 책임 원칙을 잘 지키고 있다.
