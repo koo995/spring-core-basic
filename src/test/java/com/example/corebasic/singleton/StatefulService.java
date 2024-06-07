906da52b1de8faac71af7e525116bcea46425f87
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-07 22:07:58 +0900

    불 필요한 파일 삭제

--- src/test/java/com/example/corebasic/singleton/StatefulService.java
+++
@@ -1,9 +0,0 @@
-package com.example.corebasic.singleton;
-
-public class StatefulService {
-
-    public int order(String name, int price) {
-        System.out.println("name = " + name + " price = " + price);
-        return price;
-    }
-}
