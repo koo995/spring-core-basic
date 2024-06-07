442d3a968f40c6f76d0758414df4ef7510a57fd3
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-07 20:03:28 +0900

    싱글톤 방식의 주의점 - 해결
    지금은 딱 눈에 띄지만... 실무에서 멀티스레드와 상속관계들이 엮겨있으면 찾기가 매우 어렵다

--- src/test/java/com/example/corebasic/singleton/StatefulServiceTest.java
+++ src/test/java/com/example/corebasic/singleton/StatefulServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.corebasic.singleton;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,15 +14,11 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // ThreadA: A 사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         // ThreadB: B 사용자 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
-        // ThreadA: 사용자 A 주문 금액 조회 -> 10000원이 나와야 하는데 B 사용자의 20000원이 나온다.
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
-
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        System.out.println("userAPrice = " + userAPrice);
     }
 
     static class TestConfig {
