53675ca967844982e76e944f7c467e99045b01aa
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-18 17:43:49 +0900

    조회한 빈이 모두 필요할 때, List, Map
    의도적으로 정말 해당 타입의 스프링 빈이 다 필요한 경우도 있다.
    예를들어 할인 서비스를 제공하는데 있어서 클라이언트가 할인의 종류를 선택할 수 있는 경우 스프링을 사용하면 전략패턴을 매우 간단하게 구현할 수 있다.

---
+++ src/test/java/com/example/corebasic/autowired/AllBeanTest.java
@@ -0,0 +1,56 @@
+package com.example.corebasic.autowired;
+
+import com.example.corebasic.AutoAppConfig;
+import com.example.corebasic.discount.DiscountPolicy;
+import com.example.corebasic.member.Grade;
+import com.example.corebasic.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 스프링이 빈에 등록된 애들 자동으로 쬑 들어와 가지고 다양성을 활용해 가지고 유연한 전략패턴을 활용할 수 있다.
+ */
+public class AllBeanTest {
+
+    @DisplayName("타입에 해당하는 모든 빈을 찾는다")
+    @Test
+    void findAllBean() throws Exception {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
