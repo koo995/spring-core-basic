a41452113afe22c248cd670b9d00738995193875
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-18 14:13:52 +0900

    다양한 의존관계 주입 방법 - 필드 주입

--- src/main/java/com/example/corebasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/corebasic/order/OrderServiceImpl.java
@@ -9,23 +9,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
     /**
-     * 생성자 주입은 빈을 등록하면서 의존관계가 주입이 일어난다.
-     * setter 은 빈 등록 후 의존관계가 주입된다.
-     * 특징? 1. 선택적이다. 즉, 변경 가능성이 있는 의존관계에 사용
+     * 필드 주입은
+     * 의존관계에다 그냥 넣어버리는 것이다. private 라도 가능하다.
+     * 코드가 간결하다. 하지만 안티페턴이다.
+     * 그러나 외부에서 변경이 불가능하여 테스트하기가 어렵다. -> 테스트할때 mock 객체를 이용해야 하는 경우 이 값을 바꿀 수 없다. 방법에 전혀없다.
+     * 넣을려면 setter 을 따로 만들어줘야한다. <- 그럴바에야 그냥 setter 에 @Autowired? 아예 그냥 이상하게 된다.
+     * 결론적으로 di 프레임워크가 없으면 아무것도 할 수 없다. 모든것을 컨테이너에서 가져와야 한다.
+     * 하지만 애플리케이션 코드와 관계없는 테스트에서는 사용하면 좋다. (또는 @Configuration 에서만..?)
      */
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
-    }
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
 
     /**
      * 단일 책임 원칙을 잘 지키고 있다.
