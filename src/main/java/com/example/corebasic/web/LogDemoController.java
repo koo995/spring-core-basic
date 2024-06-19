eeee7f0ec13fc730de886e02831a7c0c8fc42c1e
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-20 04:44:24 +0900

    스코프와 Provider
    덕분에 ObjectProvider.getObject() 를 호출하는 시점까지 request.scope 빈의 생성을 지연 할 수 있다.
    하지만 개발자들의 욕심은 끝이 없고... 프록시를 사용하게 된다.

--- src/main/java/com/example/corebasic/web/LogDemoController.java
+++ src/main/java/com/example/corebasic/web/LogDemoController.java
@@ -3,6 +3,7 @@ package com.example.corebasic.web;
 import com.example.corebasic.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,11 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
-    @RequestMapping("log-domo")
+    /**
+     * 오타수정 domo -> demo
+     */
+    @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
