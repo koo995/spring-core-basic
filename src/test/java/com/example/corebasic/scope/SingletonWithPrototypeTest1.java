fa45bca04e613eaaf4da46a5a282c03cd9f411f9
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-19 23:49:45 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점
    제일 간단한 방법 - 무식하게 ac을 주입받는다.

--- src/test/java/com/example/corebasic/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/com/example/corebasic/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -26,7 +27,8 @@ public class SingletonWithPrototypeTest1 {
     /**
      * 만약 우리의 의도가 이 로직을 호출할 때마다 이 프로토타입 빈을 항상 새로 만들어 쓰고 싶다면 의도한 것과 다르다
      * 프로토타입 빈을 사용한다는 것은 사용할때 그냥 계속 새로 만들어서 쓰고 싶어서 쓰는거지 이런 경우면 그냥 싱글톤을 쓰는것과 다를 바 없다.
-     * 그러면 어떻게 해야할까?
+     * 그러면 어떻게 해야할까? 제일 무식한 방법은 ac 을 주입받아서 매번 요청하는 것이다.
+     * 하지만 너무 지저분하다... 너무 스프링에 의존적이다.
      */
     @Test
     void singletonClientUsePrototype() {
@@ -37,19 +39,16 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입. 사실상 멤버변수로 값을 유지한다.
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        ApplicationContext ac;
 
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
